df8063ef7cdb5f9005b73d9c973433b284e77103
Blake Tarter <blake@Blakes-MacBook-Pro.local>
2014-11-05 00:29:37 -0600

    added default page templates and started on home page, got first animation and most of navbar working nicely.

--- app/js/app.js
+++ app/js/app.js
@@ -10,9 +10,45 @@ var bt = angular
                     templateUrl: 'views/home.html',
                     controller: 'homeController'
                 })
+                .when('/about', {
+                    templateUrl: 'views/about.html',
+                    controller: 'aboutController'
+                })
+                .when('/work', {
+                    templateUrl: 'views/work.html',
+                    controller: 'workController'
+                })
+                .when('/blog', {
+                    templateUrl: 'views/blog.html',
+                    controller: 'blogController'
+                })
                 .otherwise({
                     redirectTo: '/404'
                 });
 
             // $locationProvider.html5Mode(true);
-        }]);
+        }])
+        .run( function($rootScope, $location) {
+          $rootScope.$on('$routeChangeSuccess', function() {
+            angular.element(document).ready(function() {
+              var nav = document.getElementById('nav'),
+                  subNav = document.getElementById('sub-nav'),
+                  height = window.innerHeight,
+                  scroll;
+
+              function navCheck() {
+                scroll = document.body.scrollTop;
+
+                if (scroll > 80) {
+                  nav.className = 'nav mini';
+                } else {
+                  nav.className = 'nav';
+                }
+              }
+
+              navCheck();
+
+              window.onscroll = navCheck;
+            });
+         });
+        });
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,6 @@
-bt.controller('navController', function($scope) {
-  $scope.$watch('$viewContentLoaded', function(){
-    var nav = document.getElementById('nav');
-
-    window.onscroll = function (e) {
-      if (document.body.scrollTop > 50) {
-        nav.className = 'nav mini';
-      } else {
-        nav.className = 'nav';
-      }
-    }
+bt.controller('navController', function($scope, $location) {
+ $scope.$on('$locationChangeStart', function() {
+   $scope.selected = $location.$$url.replace('/', '');
  });
 });
 
@@ -16,6 +8,77 @@ bt.controller('footerController', function($scope) {
 
 });
 
-bt.controller('homeController', function($scope) {
+bt.controller('homeController', function($scope, $timeout) {
+  var skillset = [
+        'Javascript',
+        'AngularJS',
+        'HTML',
+        'CSS',
+        'BackboneJS',
+        'UnderscoreJS',
+        'Phonegap',
+        'Responsive Design',
+        'Git',
+        'Ajax',
+        'jQuery'
+      ],
+      s = 0,
+      sl = skillset.length;
+
+  function chooseSkillSet(init) {
+    var tempSkill;
+
+    if (s < sl) {
+      tempSkill = skillset[s++];
+    } else {
+      s = 0;
+      tempSkill = skillset[s];
+    }
+
+    if (init) {
+      $scope.skill = tempSkill;
+    } else {
+      var skillItem = document.getElementsByClassName('skill')[0];
+
+      skillItem.setAttribute('style', 'box-shadow: inset -' + skillItem.offsetWidth + 'px 0px 0px 0px rgba(0, 89, 74, 0.6);');
+
+      $timeout(function() {
+        $scope.skill = '';
+        skillItem.setAttribute('style', '');
+
+        $timeout(function() {
+          var i = 0,
+              l = tempSkill.length;
+
+          function typeLetter() {
+            if (i < l) {
+              $scope.skill += tempSkill[i];
+              i++;
+              $timeout(typeLetter, 200);
+            } else {
+              $timeout(chooseSkillSet, 3500);
+            }
+          }
+
+          $timeout(typeLetter, 200);
+        }, 500);
+      }, 1000);
+    }
+  }
+
+  chooseSkillSet(true);
+
+  $timeout(chooseSkillSet, 5000);
+});
+
+bt.controller('aboutController', function($scope) {
+
+});
+
+bt.controller('workController', function($scope) {
+
+});
+
+bt.controller('blogController', function($scope) {
 
 });
