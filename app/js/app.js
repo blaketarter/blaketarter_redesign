91f71421a3616b2c9215de67d61d25b43a54341c
Blake Tarter <blake@codekoalas.com>
2014-12-30 22:31:56 -0600

    fixed scroll stuff on firefix and ie

--- app/js/app.js
+++ app/js/app.js
@@ -36,8 +36,40 @@ var bt = angular
                   height = window.innerHeight,
                   scroll;
 
+              var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0; // Opera 8.0+ (UA detection to detect Blink/v8-powered Opera)
+              var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
+              var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0; // At least Safari 3+: "[object HTMLElementConstructor]"
+              var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
+              var isIE = /*@cc_on!@*/false || !!document.documentMode; // At least IE6
+
+              var currentPage, scrollElement; // Globals
+
+              (function findCurrentPage() {
+                currentPage = window.location.pathname;
+
+                if (currentPage.length) {
+                  currentPage = currentPage.replace(/\W+/g, '');
+
+                  if (!currentPage.length) {
+                    currentPage = 'home';
+                  }
+                } else {
+                  currentPage = window.location.href;
+                }
+              })();
+
+              (function detectScrollElement() {
+                if (isFirefox || isIE) {
+                  scrollElement = document.documentElement;
+                } else {
+                  scrollElement = document.body;
+                }
+
+                return scrollElement;
+              })();
+
               function navCheck() {
-                scroll = document.body.scrollTop;
+                scroll = scrollElement.scrollTop;
 
                 if (scroll > height) {
                   nav.className = 'nav mini';
